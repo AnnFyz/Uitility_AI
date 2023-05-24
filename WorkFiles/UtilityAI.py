b856528898daf455ef391ec57298405f0cd11657
AnnFyz <annfyz1232@yandex.ru>
2023-05-24 17:54:35 +0200

    add jeremy behavior

--- WorkFiles/UtilityAI.py
+++ WorkFiles/UtilityAI.py
@@ -1,12 +1,25 @@
+
 import csv
+import math
 
 def getUtilityScore(gamestate, action):
     u = 0
-    actionStateChanges = action[3:8]
-    gameStateStat = gamestate[3:8]
-    for a in actionStateChanges:
-        weight = -math.log(gameStateStat[i])
+    actionStateChanges = action[2:8]
+    gameStateStat = gamestate[2:8]
+    for i, actionStat in enumerate(actionStateChanges):
+        weight = -math.log(gameStateStat[i] * 0.1, 10) * 5
+        u += weight * float(actionStat)
+        #print(u)
     return u
+
+
+def applyAction(gamestate, action):
+    print(action[1])
+    for i,actionStat in enumerate(action):
+        if i < 3: #! 3 states
+            continue
+        gamestate[i] += float(actionStat)
+    return gamestate
 
 actions = []
 gamestate = ["Gamestate", "This is gameState", 999,5,5,5,5,5]
@@ -20,10 +33,17 @@ with open ("./WorkFiles\Actions.csv") as file:
             continue
         actions.append(row)
 
-#SimulationLoop
 
+
+#SimulationLoop
 for i in range(100):
     utilities = []
     for action in actions:
-        getUtilityScore(gamestate, action)
+        utilities.append(getUtilityScore(gamestate, action))
+
+    largestUtilityIndex =  utilities.index(max(utilities))
+    bestAction = actions[largestUtilityIndex]
+    gamestate = applyAction(gamestate,bestAction)
+    print(f"The largest utility index: {largestUtilityIndex}")
+
          
